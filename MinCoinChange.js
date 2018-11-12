901a3c1604eafdeb9c15abcafeb902c4cd02acd8
liangjie <605564874@qq.com>
2018-11-12 17:34:07 +0800

    add

--- MinCoinChange.js
+++ MinCoinChange.js
@@ -2,28 +2,32 @@ function MinCoinChange(coins){
     var coins=coins;
     var cache={};
     this.makeChange=function(amount){
-        var me=this;
-        if(!amount){
-            return [];
-        }
-        if(cache[amount]){
-            return cache[amount];
-        }
-        var min=[],newMin,newAmount;
-        for(var i=0;i<coins.length;i++){
-            var coin=coins[i];
-            newAmount=amount-coin;
-            if(newAmount>=0){
-                newMin=me.makeChange(newAmount);
-            } 
-            if(newAmount>=0&&(newMin.length<min.length-1||!min.length)&&
-            (newMin.length||!newAmount)){
-                min=[coin].concat(newMin);
-                console.log('new Min '+min+' for '+amount);
-            }
-        }
-        return(cache[amount]=min);
+       var me=this;
+       if(amount<=0){
+           return [];
+       }
+       if(cache[amount]){
+           return cache[amount];
+       }
+       var min=[],newMin,newAmount;
+       for(var i=0;i<coins.length;i++){
+           var coin=coins[i];
+           newAmount=amount-coin;
+           if(newAmount>=0){
+               newMin=me.makeChange(newAmount);
+           }
+           if(
+               newAmount>=0&&
+               (newMin.length<min.length-1||!min.length)
+               &&(newMin.length||!newAmount)
+           ){
+             min=[coin].concat(newMin);
+           }
+       }
+       return (cache[amount]=min);
     };
-}
+    this.getCache=function(){
+      return cache;
+    }
+};
 var minCoinChange=new MinCoinChange([1,5,10,25]);
-console.log(minCoinChange.makeChange(36));
